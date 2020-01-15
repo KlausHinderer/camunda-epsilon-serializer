af596cca4889b5fb0d38e48fb429c6e16250ffed
Klaus Hinderer <kontakt@klaushinderer.de>
2020-01-15 19:50:36 +0100

    Test improved

--- src/test/java/de/metaphisto/camundaserialize/Ausgabe.java
+++ src/test/java/de/metaphisto/camundaserialize/Ausgabe.java
@@ -2,18 +2,14 @@ package de.metaphisto.camundaserialize;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
-import org.camunda.bpm.engine.variable.impl.value.ObjectValueImpl;
 
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectOutputStream;
-import java.util.Date;
+import static org.junit.Assert.assertEquals;
 
 public class Ausgabe implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        System.out.println(execution.getVariable("datum"));
-        System.out.println(execution.getVariable("vektor"));
-        System.out.println(execution.getVariable("zahl"));
+        assertEquals(PluginTest.JETZT, execution.getVariable("datum"));
+        assertEquals(PluginTest.VECTOR, execution.getVariable("vektor"));
+        assertEquals(PluginTest.VALUE, execution.getVariable("zahl"));
     }
 }
--- src/test/java/de/metaphisto/camundaserialize/PluginTest.java
+++ src/test/java/de/metaphisto/camundaserialize/PluginTest.java
@@ -2,7 +2,6 @@ package de.metaphisto.camundaserialize;
 
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.runtime.Job;
-import org.camunda.bpm.engine.runtime.ProcessInstanceWithVariables;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
@@ -22,18 +21,20 @@ public class PluginTest {
 
     @Rule
     public ProcessEngineRule processEngineRule = new ProcessEngineRule();
+    static final Date JETZT = new Date(System.currentTimeMillis() - 3600000);
+    static final List<Object> VECTOR = new Vector<>();
+    static final long VALUE = 1L;
 
     @Test
-    @Deployment(resources = { "async.bpmn" })
+    @Deployment(resources = {"async.bpmn"})
     public void testBpmnParseListener() throws IOException {
-        Map<String,Object> variables = new HashMap<>();
-        Date jetzt = new Date(System.currentTimeMillis()-3600000);
-        List<Object> vector = new Vector<>();
-        vector.add("Teststring in Vector");
-        variables.put("datum",jetzt);
-        variables.put("vektor",vector);
-        variables.put("zahl", 1L);
-        ProcessInstanceWithVariables process = processEngineRule.getRuntimeService().createProcessInstanceByKey("Process_async").setVariables(variables).executeWithVariablesInReturn();
+        //This test starts a process. The process reaches a UserTask, where values are serialized and deserialized.
+        Map<String, Object> variables = new HashMap<>();
+        VECTOR.add("Teststring in Vector");
+        variables.put("datum", JETZT);
+        variables.put("vektor", VECTOR);
+        variables.put("zahl", VALUE);
+        processEngineRule.getRuntimeService().createProcessInstanceByKey("Process_async").setVariables(variables).executeWithVariablesInReturn();
 
         TaskService taskService = processEngineRule.getTaskService();
         Task task = taskService.createTaskQuery().singleResult();
